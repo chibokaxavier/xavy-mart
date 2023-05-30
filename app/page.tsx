bc28a2b90a4fdf6fd41b353d56e1bab79c3da175
manlikexavy <chibokaxavier@gmail.com>
2023-05-30 09:08:43 -0700

    PRODUCT COMPONENT CREATED

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,36 @@
-import Image from 'next/image'
-
-export default function Home() {
-  return (
-  
  
-  )
+
+import Head from "next/head";
+
+export default async function Home() {
+  const res = await fetch ("https://fakestoreapi.com/products")
+  const products: Product[]= await res.json() 
+  return (
+    <>
+    <Head>
+      <title>
+        Xavy-Mart
+      </title>
+    </Head>
+      <main className="">
+        {" "}
+        <section className="flex flex-col space-y-12 pb-44 ">
+          <h1 className="text-5xl font-bold text-center">DEALS OF THE DAY</h1>
+          {
+            products.map((product)=>(
+              <div key={product.id} className="">
+               <h1>{product.description}</h1>
+              </div>
+            ))
+          }
+          <div>
+            {/* products */} 
+            {/* products */}
+            {/* products */}
+            {/* products */}
+          </div>
+        </section>{" "}
+      </main>
+    </>
+  );
 }
---
+++ app/product/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductPage = () => {
+  return (
+    <div>Product oage</div>
+  )
+}
+
+export default ProductPage
