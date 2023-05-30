d276f7ccba2caf20a1d2799c165f877bff85af16
manlikexavy <chibokaxavier@gmail.com>
2023-05-30 10:51:22 -0700

    RESPONSIVENESS  ON CHECK

--- app/page.tsx
+++ app/page.tsx
@@ -13,15 +13,18 @@ export default async function Home() {
         Xavy-Mart
       </title>
     </Head>
-      <main className="">
+      <main className=" min-h-screen px-8 max-w-7xl  mx-auto xl:px-0 mt-48">
         {" "}
         <section className="flex flex-col space-y-12 pb-44 ">
           <h1 className="text-5xl font-bold text-center">DEALS OF THE DAY</h1>
+          <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  xl:gap-x-8">
           {
             products.map((product)=>(
               <Product key={product.id} product={product}/>
             ))
           }
+          </div>
+         
           <div>
             {/* products */} 
             {/* products */}
